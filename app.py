b721196ef9695163f590fadd971fb5eff9db84b8
Pzhoon <120434460+Pzhoon@users.noreply.github.com>
2022-12-15 19:54:36 +0900

    1

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import joblib
 import os
 
 model_path = f"{os.path.dirname(os.path.abspath(__file__))}/first_model.pkl"
-model = joblib.load(model_path)
+model = joblib.load(first_model_path)
 st.write("## 선형 회귀 모델")
 st.write(pd.Series(model.coef_, index=["age", "bmi", "children", "smoker", "sex_male", "region_northwest", "region_northeast", "region_southwest"]))
 
