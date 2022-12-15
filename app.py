a0854d4dc2e82c946f7457137a2b4d73a5d7be63
Pzhoon <120434460+Pzhoon@users.noreply.github.com>
2022-12-15 19:58:29 +0900

    1

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 df = pd.read_csv('https://raw.githubusercontent.com/bigdata-young/ai_26th/main/data/insurance.csv')
 st.write(df)
 
+pip install joblib
 import joblib
 import os
 
