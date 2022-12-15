74b585ee86efa5ff6e296f41eade38a5767d3fe4
Pzhoon <120434460+Pzhoon@users.noreply.github.com>
2022-12-15 20:00:18 +0900

    1

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 df = pd.read_csv('https://raw.githubusercontent.com/bigdata-young/ai_26th/main/data/insurance.csv')
 st.write(df)
 
+from joblib import Parallel, delayed
 import joblib
 import os
 
